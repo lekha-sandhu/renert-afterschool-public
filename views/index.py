1a060c07b5d30a175793a2aa54abadd2d7d60648
Assaf Gordon <softwaredev@housegordon.com>
2024-09-20 20:21:34 +0000

    Index page: show name of random staff member

--- views/index.py
+++ views/index.py
@@ -6,15 +6,21 @@ from pprint import pprint,pformat
 from sqlalchemy import text
 import sqlalchemy.exc
 import json
+from  sqlalchemy.sql.expression import func
 
 from flask import request, render_template, redirect, url_for, abort, session
 from flask import flash, current_app
 
 from root import app,db
 
+from models.students import Student
 import global_settings
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    # Get name of a random staf member
+    s = Student.query.filter_by(grade='Staff').order_by(func.random()).first()
+    pprint(s)
+    return render_template("index.html",
+                           random_person=s)
