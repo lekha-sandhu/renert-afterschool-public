f086617785413749704234f74ffe32c30eb96967
FinnE145 <finn.emmerson.12@gmail.com>
2025-05-23 11:34:16 -0600

    Flask Admin: Create afterschool_enrollment view

--- views/afterschool_admin.py
+++ views/afterschool_admin.py
@@ -25,12 +25,19 @@ from hgsc_utils.flask_admin.flask_admin_permission_mixin import RenertAdminPermi
 
 from models.afterschool_classes import AfterschoolClass
 from models.afterschool_signins import AfterschoolSignin
+from models.afterschool_enrollment import AfterschoolEnrollment
 
 def _format_datetime(dt):
-    return dt.strftime("%Y-%m-%d %I:%M:%S %p").replace(" 0", " ") if dt else None
+    return dt.strftime("%Y-%m-%d if  %I:%M:%S %p").replace(" 0", " ") if dt else None
+
+def _format_date(dt):
+    return dt.strftime("%Y-%m-%d") if dt else None
 
 def _format_time(dt):
     return dt.strftime("%I:%M:%S %p").lstrip("0") if dt else None
+
+def _format_afterschool_class(ac):
+    return Markup(f'<a href="/admin/afterschoolclass/?search={(ac.activity + " " + ac.room).replace(" ", "+")}">{ac.activity} ({ac.room})</a>')
 
 class AfterschoolAdminPermissionMixin(RenertAdminPermissionMixin):
     needed_permission = "afterschool_admin"
@@ -118,11 +125,8 @@ class AfterschoolSigninsView(AfterschoolAdminPermissionMixin,ModelView):
         'sign_in_date_cache'
     ]
 
-    def _format_afterschool_class(view, context, model, name):
-        return Markup(f'<a href="/admin/afterschoolclass/?search={(model.afterschool_class.activity + " " + model.afterschool_class.room).replace(' ', '+')}">{model.afterschool_class.activity} ({model.afterschool_class.room})</a>')
-
     column_formatters = {
-        'afterschool_class': _format_afterschool_class,
+        'afterschool_class': lambda v,c,m,n: _format_afterschool_class(m.afterschool_class),
         'sign_in_time': lambda v,c,m,n: _format_datetime(m.sign_in_time),
         'sign_out_time': lambda v,c,m,n: _format_datetime(m.sign_out_time)
     }
@@ -138,5 +142,40 @@ class AfterschoolSigninsView(AfterschoolAdminPermissionMixin,ModelView):
         ("created_at",True),
     ]
 
+class AfterschoolEnrollmentView(AfterschoolAdminPermissionMixin,ModelView):
+    can_create = True
+    can_delete = False
+    can_edit = True
+    can_export = True
+
+    column_searchable_list = [
+        'student.name',
+        'afterschool_class.activity',
+    ]
+
+    column_filters = [
+        'afterschool_class',
+        'student',
+        'start_date',
+        'end_date'
+    ]
+
+    column_formatters = {
+        'afterschool_class': lambda v,c,m,n: _format_afterschool_class(m.afterschool_class),
+        'start_date': lambda v,c,m,n: _format_date(m.start_date),
+        'end_date': lambda v,c,m,n: _format_date(m.end_date)
+    }
+
+    column_exclude_list = [
+        "created_at",
+        "updated_at",
+    ]
+
+    column_default_sort = [
+        ("updated_at",True),
+        ("created_at",True),
+    ]
+
 admin.add_view(AfterschoolClassesView(AfterschoolClass, db.session, name="AfterSchool Classes",category="Afterschool"))
 admin.add_view(AfterschoolSigninsView(AfterschoolSignin, db.session, name="AfterSchool Sign-ins",category="Afterschool"))
+admin.add_view(AfterschoolEnrollmentView(AfterschoolEnrollment, db.session, name="AfterSchool Enrollment",category="Afterschool"))
