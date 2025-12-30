2893100f04ab86a386d2edea48b4fe723b27a741
Assaf Gordon <assafgordon@gmail.com>
2025-12-30 09:21:03 +0000

    DB: Afterschool-enrollment: use PostgreSQL GiST constraint to prevent overlapping enrollemnts

--- models/afterschool_enrollment.py
+++ models/afterschool_enrollment.py
@@ -1,6 +1,10 @@
-from sqlalchemy import text,Computed
-from root import db
+from sqlalchemy import text,Computed,func
+from sqlalchemy.dialects.postgresql import ExcludeConstraint
+
+
 from hgsc_utils.sqla.timestamp_mixin import TimestampMixin
+from root import db
+
 
 class AfterschoolEnrollment(db.Model, TimestampMixin):
     __tablename__ = "afterschool_enrollments"
@@ -15,6 +19,33 @@ class AfterschoolEnrollment(db.Model, TimestampMixin):
 
     start_date = db.Column(db.DATE,nullable=False)
     end_date = db.Column(db.DATE,nullable=True)
+
+
+    __table_args__ = (
+        ## This beauty is an EXCLUDE constraint that prevents the same student_id,
+        ## afterschool_class_id, in overlapping dates.
+        ##
+        ## The equilvanent SQL command is:
+        ##
+        ##    ALTER TABLE afterschool_enrollments
+        ##       ADD CONSTRAINT no_overlapping_enrollments
+        ##       EXCLUDE USING gist (
+        ##          student_id WITH =,
+        ##          afterschool_class_id WITH =,
+        ##          daterange(start_date, COALESCE(end_date, 'infinity'::date), '[]') WITH &&
+        ##       );
+        ##
+        ## Don't forget to enable gist/btree first (just once):
+        ##
+        ##     CREATE EXTENSION IF NOT EXISTS btree_gist;
+        ExcludeConstraint(
+            (student_id, '='),
+            (afterschool_class_id, '='),
+            (func.daterange(start_date, func.coalesce(end_date, 'infinity'), '[]'), '&&'),
+            name='no_overlapping_enrollments',
+            using='gist'
+        ),
+    )
 
     def __repr__(self):
         return f"Afterschool-Enrollment<id: {self.afterschool_enrollment_id}>"
